12477bb281a70fbac2393c4fec10dfca2c851c5e
Xavi Gallardo <xavigallardo@gmail.com>
2018-06-28 12:53:19 +0200

    Added FinalPRice

--- js/main.js
+++ js/main.js
@@ -9,13 +9,18 @@ console.log(calculateButton);
 
 calculateButton.onclick = function() {
     var producto = document.getElementsByClassName('producto');
+    var totalTodos = 0;
     for (var i = 0; i < producto.length; i++) {
         var price = document.getElementsByClassName('price')[i].innerText;
         var inputNumber = document.getElementsByTagName('input')[i].value;
         var total = price * inputNumber;
+        totalTodos += total;
         var totalPriceProduct = document.getElementsByClassName('totalPriceProduct')[i];
         totalPriceProduct.innerHTML = total;
     }
+    var totalPrize = document.getElementById('TotalPrize');
+    totalPrize.innerHTML = totalTodos;
+
 
 };
 
